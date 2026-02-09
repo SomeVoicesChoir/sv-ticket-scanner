41dd8757c323e658bca25c3424ab1c1b5ec21144
SomeVoicesChoir <curtis@somevoices.co.uk>
2026-02-09 10:44:50 +0000

    Update get-events.js

--- api/get-events.js
+++ api/get-events.js
@@ -44,6 +44,8 @@ module.exports = async function handler(req, res) {
             name: record.fields['Event Name'] || 'Unnamed Event',
             displayName: record.fields['Display Name'] || record.fields['Event Name'] || 'Unnamed Event',
             showName: record.fields['Event Name (input here)'] || '',
+            showDescription: record.fields['Event Description'] || '',
+showImage: (record.fields['Event Image'] && record.fields['Event Image'][0]) ? record.fields['Event Image'][0].url : '',
             ticketTypePrice: record.fields['Ticket Type + Price'] || 'Standard', // ✅ This is needed
             price: record.fields['Ticket Price'] || 0,
             stripePriceId: record.fields['Stripe Price ID'],
